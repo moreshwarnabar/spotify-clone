e20105fa87b1a606abfba112017b940aca5f74ca
moreshwarnabar <mrnabar@gmail.com>
2024-03-27 16:51:39 -0700

    added sign up and log in buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { BiSearch } from 'react-icons/bi';
 import { HiHome } from 'react-icons/hi';
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { twMerge } from 'tailwind-merge';
+import Button from './Button';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -48,7 +49,25 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign Up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log In
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
