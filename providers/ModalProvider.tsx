8b0ed5b0951964be1b9b06f0fb77144422e0caf0
moreshwarnabar <mrnabar@gmail.com>
2024-03-28 09:35:05 -0700

    updated Modal by using radix dialog component

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -17,7 +17,14 @@ const ModalProvider = () => {
 
   return (
     <>
-      <Modal />
+      <Modal
+        title="Test Modal"
+        description="Test Desc"
+        isOpen
+        onChange={() => {}}
+      >
+        Test Children
+      </Modal>
     </>
   );
 };
