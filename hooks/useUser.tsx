39330e39f7ea80f043bc4e6538c8cde1a3716fa0
moreshwarnabar <mrnabar@gmail.com>
2024-03-28 18:38:20 -0700

    resolved error due to no record present in db

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -35,13 +35,13 @@ export const MyUserContextProvider = (props: Props) => {
   const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
   const [subscription, setSubscription] = useState<Subscription | null>(null);
 
-  const getUserDetails = () => supabase.from('users').select('*').single();
+  const getUserDetails = () => supabase.from('users').select('*').maybeSingle();
   const getSubscription = () =>
     supabase
       .from('subscriptions')
       .select('*, prices(*, products(*))')
       .in('status', ['trialing', 'active'])
-      .single();
+      .maybeSingle();
 
   useEffect(() => {
     if (user && !isLoadingData && !userDetails && !subscription) {
